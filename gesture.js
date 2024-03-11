9d24c5af76d7be37cd4b26e10362bf47bc6ef88f
morikawa kaoru <128456055+84kaoru@users.noreply.github.com>
2024-03-11 14:12:30 +0900

    完成

--- gesture.js
+++ gesture.js
@@ -1,46 +1,49 @@
-const video = document.getElementById("input");
-const canvas = document.getElementById("output");
-const ctx = canvas.getContext("2d");
 
-const config = {
-    locateFile: file =>
-    `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
-};
+    const video = document.getElementById('input');
+    const canvas = document.getElementById('output');
+    const ctx = canvas.getContext('2d');
+    
+    //関連ファイルの読み込み
+    const config = {
+      locateFile: file => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+    };
+    const hands = new Hands(config);
 
-const hands = new Hands(config);
-
-const camera = new Camera(video, {
-    onFrame: async () =>{
+    //カメラからの映像をhands.jsで使えるようにする
+    const camera = new Camera(video, {
+      onFrame: async () => {
         await hands.send({image: video});
-    },
-    width: 600,
-    height: 400
-});
+      },
+      width: 600,
+      height: 400
+    });
 
-hands.setOptions({
-    maxNumHands: 2,
-    modeComplexity: 1,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
-});
+    hands.setOptions({
+        maxNumHands: 2,              //検出する手の最大数
+        modelComplexity: 1,          //ランドマーク検出精度(0か1)
+        minDetectionConfidence: 0.5, //手を検出するための信頼値(0.0~1.0)
+        minTrackingConfidence: 0.5   //ランドマーク追跡の信頼度(0.0~1.0)
+    });
 
-document.getElementById("start")
-.addEventListener("click", () => camera.start());
-
-document.getElementById("stop")
-.addEventListener("click", () => camera.stop());
-
-hands.onResults(results => {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.drawImage(results.image,0,0,canvas.width,canvas.height);
-
-    if(results.multiHandLandmarks) {
+    //形状認識した結果の取得
+    hands.onResults(results => {
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      ctx.drawImage(results.image,0,0,canvas.width,canvas.height);
+      
+      if(results.multiHandLandmarks) {
         results.multiHandLandmarks.forEach(marks => {
-            drawConnectors(ctx, marks, HAND_CONNECTIONS, {color: "#0f0"});
+          // 緑色の線で骨組みを可視化
+          drawConnectors(ctx, marks, HAND_CONNECTIONS, {color: '#0f0'});
 
-            drawLandmarks(ctx, marks, {color: "f00"});
-
+          // 赤色でランドマークを可視化
+          drawLandmarks(ctx, marks, {color: '#f00'});
         })
-    }
+      }
+    });
 
-});
+    
+    //認識開始・終了ボタン
+    document.getElementById('start')
+      .addEventListener('click', () => camera.start());
+    document.getElementById('stop')
+      .addEventListener('click', () =>  camera.stop());
